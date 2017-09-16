62d534680c426f97b7987f489dfb0496f3d00b72
diracFermion <souraviitm@gmail.com>
2017-09-16 16:19:55 -0400

    constraining 2 columns on right end to slide along x only

--- HOOMD_python/ribbon.py
+++ HOOMD_python/ribbon.py
@@ -7,7 +7,7 @@ import sys
 import random
 
 hoomd.context.initialize()
-s = hoomd.init.read_gsd("init_strip.gsd")
+s = hoomd.init.read_gsd("../../Sim_dump_ribbon/init_strip.gsd")
 
 for p in s.particles:
 	if p.type == 'A':
@@ -19,20 +19,25 @@ harmonic = md.bond.harmonic()
 dih = md.dihedral.harmonic()
 
 dih.dihedral_coeff.set('A', k=4.000, d=1, n=1)
+dih.dihedral_coeff.set('D', k=4.000, d=1, n=1)
 
 harmonic.bond_coeff.set('A', k=800.000, r0=1.0)
+harmonic.bond_coeff.set('D', k=800.000, r0=1.0)
 
-
-hoomd.analyze.log(filename="Observable.log", quantities=["temperature", "potential_energy","bond_harmonic_energy","kinetic_energy","dihedral_harmonic_energy"], period=5000, header_prefix="#", overwrite=True)
+hoomd.analyze.log(filename="../../Sim_dump_ribbon/Observable.log", quantities=["temperature", "potential_energy","bond_harmonic_energy","kinetic_energy","dihedral_harmonic_energy"], period=5000, header_prefix="#", overwrite=True)
 
 md.integrate.mode_standard(dt=0.0010)
 
 group1 = hoomd.group.type(name='group1', type='A')
+group2 = hoomd.group.type(name='group1', type='D')
+group12 = hoomd.group.union(name='group12',a=group1,b=group2)
 
-hoomd.dump.gsd(filename="trajectory.gsd", group=group.all(), period=5000, overwrite=True)
+md.constrain.oneD(group=group2, constraint_vector=[1,0,0])
+
+hoomd.dump.gsd(filename="../../Sim_dump_ribbon/trajectory.gsd", group=group.all(), period=5000, overwrite=True)
 
 
-md.integrate.nvt(group=group1,kT=1.0, tau=0.2)
+md.integrate.nvt(group=group12,kT=1.0, tau=0.2)
 
 hoomd.run(2e6)
 
