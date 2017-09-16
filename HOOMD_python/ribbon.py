d7627680d1f661b4df4625c849c03ab8fbddb9d3
diracFermion <souraviitm@gmail.com>
2017-09-16 19:48:21 -0400

    ribbon with free slider

--- HOOMD_python/ribbon.py
+++ HOOMD_python/ribbon.py
@@ -20,24 +20,28 @@ dih = md.dihedral.harmonic()
 
 dih.dihedral_coeff.set('A', k=4.000, d=1, n=1)
 dih.dihedral_coeff.set('D', k=4.000, d=1, n=1)
+dih.dihedral_coeff.set('E', k=4.000, d=1, n=1)
 
 harmonic.bond_coeff.set('A', k=800.000, r0=1.0)
 harmonic.bond_coeff.set('D', k=800.000, r0=1.0)
+harmonic.bond_coeff.set('E', k=800.000, r0=1.0)
 
 hoomd.analyze.log(filename="../../Sim_dump_ribbon/Observable.log", quantities=["temperature", "potential_energy","bond_harmonic_energy","kinetic_energy","dihedral_harmonic_energy"], period=5000, header_prefix="#", overwrite=True)
 
 md.integrate.mode_standard(dt=0.0010)
 
 group1 = hoomd.group.type(name='group1', type='A')
-group2 = hoomd.group.type(name='group1', type='D')
+group2 = hoomd.group.type(name='group2', type='D')
+group3 = hoomd.group.type(name='group3', type='E')
 group12 = hoomd.group.union(name='group12',a=group1,b=group2)
+group123 = hoomd.group.union(name='group123',a=group12,b=group3)
 
 md.constrain.oneD(group=group2, constraint_vector=[1,0,0])
 
-hoomd.dump.gsd(filename="../../Sim_dump_ribbon/trajectory.gsd", group=group.all(), period=5000, overwrite=True)
+hoomd.dump.gsd(filename="../../Sim_dump_ribbon/trajectory1.gsd", group=group.all(), period=5000, overwrite=True)
 
 
-md.integrate.nvt(group=group12,kT=1.0, tau=0.2)
+md.integrate.nvt(group=group123,kT=1.0, tau=0.2)
 
-hoomd.run(2e6)
+hoomd.run(1e6)
 
