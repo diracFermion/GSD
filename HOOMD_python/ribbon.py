751debcb25a4aef9da733f308ed04166ec7323da
diracFermion <souraviitm@gmail.com>
2017-09-16 20:30:46 -0400

    HOOMD launch generalised with dump file naming automated

--- HOOMD_python/ribbon.py
+++ HOOMD_python/ribbon.py
@@ -6,6 +6,18 @@ import gsd.hoomd
 import sys
 import random
 
+k=20.000 #Kappa
+e=1440.000*k/2 #Epsilon
+nx=100
+ny=11
+Run=1
+
+print "Kappa = ",k,"Epsilon = ",e
+obser_file = '../../Sim_dump_ribbon/observable_k'+str(k)+'_r'+str(Run)+'.log'
+print "Observable data is dumped in file: ",obser_file
+traj_file = '../../Sim_dump_ribbon/trajectory_k'+str(k)+'_r'+str(Run)+'.gsd'
+
+
 hoomd.context.initialize()
 s = hoomd.init.read_gsd("../../Sim_dump_ribbon/init_strip.gsd")
 
@@ -18,15 +30,15 @@ for p in s.particles:
 harmonic = md.bond.harmonic()
 dih = md.dihedral.harmonic()
 
-dih.dihedral_coeff.set('A', k=4.000, d=1, n=1)
-dih.dihedral_coeff.set('D', k=4.000, d=1, n=1)
-dih.dihedral_coeff.set('E', k=4.000, d=1, n=1)
+dih.dihedral_coeff.set('A', k=k, d=1, n=1)
+dih.dihedral_coeff.set('D', k=k, d=1, n=1)
+dih.dihedral_coeff.set('E', k=k, d=1, n=1)
 
-harmonic.bond_coeff.set('A', k=800.000, r0=1.0)
-harmonic.bond_coeff.set('D', k=800.000, r0=1.0)
-harmonic.bond_coeff.set('E', k=800.000, r0=1.0)
+harmonic.bond_coeff.set('A', k=e, r0=1.0)
+harmonic.bond_coeff.set('D', k=e, r0=1.0)
+harmonic.bond_coeff.set('E', k=e, r0=1.0)
 
-hoomd.analyze.log(filename="../../Sim_dump_ribbon/Observable.log", quantities=["temperature", "potential_energy","bond_harmonic_energy","kinetic_energy","dihedral_harmonic_energy"], period=5000, header_prefix="#", overwrite=True)
+hoomd.analyze.log(filename=obser_file, quantities=["temperature", "potential_energy","bond_harmonic_energy","kinetic_energy","dihedral_harmonic_energy"], period=5000, header_prefix="#", overwrite=True)
 
 md.integrate.mode_standard(dt=0.0010)
 
@@ -38,10 +50,10 @@ group123 = hoomd.group.union(name='group123',a=group12,b=group3)
 
 md.constrain.oneD(group=group2, constraint_vector=[1,0,0])
 
-hoomd.dump.gsd(filename="../../Sim_dump_ribbon/trajectory1.gsd", group=group.all(), period=5000, overwrite=True)
+hoomd.dump.gsd(filename=traj_file, group=group.all(), period=5000, overwrite=True)
 
 
 md.integrate.nvt(group=group123,kT=1.0, tau=0.2)
 
-hoomd.run(1e6)
+hoomd.run(3e6)
 
